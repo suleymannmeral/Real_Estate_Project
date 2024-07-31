4f6ca54733fe064424a871e6a69e94740147a052
suleymannmeral <mrlslymn02@gmail.com>
2024-07-31 09:48:17 +0300

    SubFeature Repositories Added

---
+++ RealEstate_Dapper_Api/Repositories/SubFeatureRepositories/ISubFeatureRepository.cs
@@ -0,0 +1,10 @@
+﻿using RealEstate_Dapper_Api.Dtos.SubFeatureDtos;
+
+namespace RealEstate_Dapper_Api.Repositories.SubFeatureRepositories
+{
+    public interface ISubFeatureRepository
+    {
+        Task<List<ResultSubFeatureDto>> GetSubFeaturesAsync();
+
+    }
+}
